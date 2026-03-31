69464c373e49e69ad3fd4ea6e8a07d86b8c435de
phuocnv1 <vinhphuoc1892@gmail.com>
2026-03-31 10:57:25 +0700

    update code fix

--- lib/main.dart
+++ lib/main.dart
@@ -116,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment +++++++++++++++++',
+        tooltip: 'Increment +++++++++++++++++ abc dé c',
         child: const Icon(Icons.add),
       ),
     );
