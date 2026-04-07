af2b0da55b432c041c621dfadcf9c74c192d76b7
phuocnv1 <vinhphuoc1892@gmail.com>
2026-04-07 09:58:08 +0700

    gix

--- lib/main.dart
+++ lib/main.dart
@@ -116,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment +++++++++++++++++ abc dé c',
+        tooltip: 'Increment +++++++++++++++++ abcádasdasdas dé c',
         child: const Icon(Icons.add),
       ),
     );
